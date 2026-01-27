4bf0d286cc2437b4ac55299266a1af5b5550a40d
paknux <62690101+paknux@users.noreply.github.com>
2026-01-28 06:05:04 +0700

    Refactor S3 configuration in config.php
    Updated comments for clarity and adjusted bucket name description.

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 require 'vendor/autoload.php';
 use Aws\S3\S3Client;
 
-$bucket = 'nugwebphps3';
+$bucket = 'nugwebphps3';  // Sesuaikan dengan nama bucket S3
 $region = 'us-east-1'; // Sesuaikan dengan region AWS Academy
 
 $s3 = new S3Client([
@@ -10,4 +10,5 @@ $s3 = new S3Client([
     'region'  => $region
     // Tidak perlu key/secret karena kita pakai IAM Role di EC2
 ]);
+
 ?>
